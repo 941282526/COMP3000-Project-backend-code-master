4edc77d27d4f3a63a2375da3d1e015ce64cd0fc3
yg <527871441@qq.com>
2023-05-09 09:36:25 +0800

    项目初始化

--- src/main/java/com/example/jingdong/RestaurantAssistantApplication.java
+++ src/main/java/com/example/jingdong/RestaurantAssistantApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JingdongApplication {
+public class RestaurantAssistantApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JingdongApplication.class, args);
+        SpringApplication.run(RestaurantAssistantApplication.class, args);
     }
 
 }
